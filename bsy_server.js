dea049dd0f84592f15680e54d289c52f0f698d16
behoyh <beshoyhanna@outlook.com>
2023-07-04 14:14:53 -0700

    Code to return token to client

---
+++ bsy_server.js
@@ -0,0 +1,28 @@
+// Bring in environment secrets through dotenv
+require('dotenv/config')
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
+
+// Use the request module to make HTTP requests from Node
+const request = require('request')
+
+// Run the express app
+const express = require('express')
+var cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+app.get('/token', (req, res) => {
+    token = myCache.get("token");
+    if (token) {
+        return res.send(token);
+    }
+    request.get('http://localhost:4000/token', (error, response, body) => { 
+        console.log(body);
+        myCache.set("token", body, 10000000);
+        return res.send(body);
+    });
+});
+
+app.listen(8080, () => console.log(`app listening at PORT: 8080`))
