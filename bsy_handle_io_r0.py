a8e67d93983401ca9a63fae800a39d94ef4faea1
radxa <bsymain@gmail.com>
2023-12-06 19:12:13 -0800

    Got LED working for radxa

--- bsy_handle_io_r0.py
+++ bsy_handle_io_r0.py
@@ -2,7 +2,7 @@
                                 
 import signal                   
 import sys
-import RPi.GPIO as GPIO
+import os
 
 import datetime
 import time
@@ -12,51 +12,25 @@ global start
 main_dir = "/home/pi/Documents/bsy-wifi/"
 
 BUTTON_GPIO = 12
-R_LED_GPIO = 21
-G_LED_GPIO = 20
-B_LED_GPIO = 16
+R_LED_GPIO = 7
+G_LED_GPIO = 5
+B_LED_GPIO = 3
 
-def signal_handler(sig, frame):
-    GPIO.cleanup()
-    sys.exit(0)
-    
-def button_callback(channel):
-    val_lst = []
-    for i in range(9):
-        val_lst.append(GPIO.input(BUTTON_GPIO))
-        time.sleep(0.001)
-        
-    # print(val_lst)
-    if sum(val_lst)>=5:
-        val = 1
-    else:
-        val = 0
-    
-    global start
-    if not val:
-        start = datetime.datetime.now()
-        print("Button pressed!")
-    else:
-        end = datetime.datetime.now()
-        elapsed = end - start
-        print("Button released!")
-        print(elapsed.total_seconds())
-        with open(main_dir + 'tmp/button_pressed.txt','w') as f:
-            f.write(str(elapsed.total_seconds()))
+
+def GPIO_output(pin,val):
+     os.system("sudo mraa-gpio  set {}  {}".format(pin,int(val)))
+
             
         
 if __name__ == '__main__':
     global start 
     start = datetime.datetime.now()
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
     
-    GPIO.setup(R_LED_GPIO, GPIO.OUT)
-    GPIO.setup(G_LED_GPIO, GPIO.OUT)
-    GPIO.setup(B_LED_GPIO, GPIO.OUT)
+    GPIO_output(R_LED_GPIO, False)
+    GPIO_output(G_LED_GPIO, False)
+    GPIO_output(B_LED_GPIO, False)
     
-    GPIO.add_event_detect(BUTTON_GPIO, GPIO.BOTH, 
-            callback=button_callback, bouncetime=10)
     
      
 
@@ -67,23 +41,20 @@ if __name__ == '__main__':
             m = f.read()[0]
                 
         if c== '1':
-            GPIO.output(R_LED_GPIO, True)
-            GPIO.output(G_LED_GPIO, False)
-            GPIO.output(B_LED_GPIO, False)
+            GPIO_output(R_LED_GPIO, True)
+            GPIO_output(G_LED_GPIO, False)
+            GPIO_output(B_LED_GPIO, False)
         elif c== '0' and m=='0':
-            GPIO.output(R_LED_GPIO, False)
-            GPIO.output(G_LED_GPIO, False)
-            GPIO.output(B_LED_GPIO, True)
+            GPIO_output(R_LED_GPIO, False)
+            GPIO_output(G_LED_GPIO, False)
+            GPIO_output(B_LED_GPIO, True)
         elif m== '1':
-            GPIO.output(R_LED_GPIO, False)
-            GPIO.output(G_LED_GPIO, True)
-            GPIO.output(B_LED_GPIO, False)
+            GPIO_output(R_LED_GPIO, False)
+            GPIO_output(G_LED_GPIO, True)
+            GPIO_output(B_LED_GPIO, False)
         else:
-            GPIO.output(R_LED_GPIO, False)
-            GPIO.output(G_LED_GPIO, False)
-            GPIO.output(B_LED_GPIO, False)
+            GPIO_output(R_LED_GPIO, False)
+            GPIO_output(G_LED_GPIO, False)
+            GPIO_output(B_LED_GPIO, False)
         time.sleep(0.5)
 
-    
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.pause()
